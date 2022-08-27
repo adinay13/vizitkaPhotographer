55070eab455277c2bd058ae28d301e21f3feab11
adinay13 <adinay131000@gmail.com>
2022-08-27 17:56:09 +0600

    «soobsheniye»

--- src/components/footer/Map.js
+++ src/components/footer/Map.js
@@ -3,19 +3,19 @@ import DG from '2gis-maps'
 
 const Map = () => {
     useEffect(() => {
-        var map;
+        let map;
         DG.then(function () {
             map = DG.map('map', {
                 center: [42.852193254971176, 74.61646845506412],
                 zoom: 15
             });
             DG.marker([42.852193254971176, 74.61646845506412]).addTo(map).bindPopup('bruh');
-            DG.marker([42.852193254971176, 74.61646845506412]).addTo(map).bindPopup('bruh');
+            // DG.marker([42.852193254971176, 74.61646845506412]).addTo(map).bindPopup('bruh');
         });
     }, [])
     return (
         <div>
-            <div style={ { width: 340, height: 200, borderRadius: 12 } } id="map"></div>
+            <div style={ { width: 340, height: 200, borderRadius: 12 } } id="map">map</div>
         </div>
     );
 };
